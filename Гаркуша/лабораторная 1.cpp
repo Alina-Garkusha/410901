7d4ca6df6ac430e2f49df7f518924a64bae9a066
Alina-Garkusha <garkushaalina423@gmail.com>
2024-09-19 19:18:18 +0300

    Add files via upload

---
+++ Гаркуша/лабораторная 1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	float a, s, S, V;
+	cout << "Введите значение ребра куба" << endl;
+	cin >> a;
+	s = pow(a,2);
+	S = a * a * 6;
+	V = pow(a,3);
+	cout << "s = " << s << "\nS = " << S << "\nV = " << V << endl;
+	return 0;
+}
